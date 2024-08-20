594d9d1ab58857d6df6a36e26d786ad80e2d5396
Braincheru <cherubryant@gmail.com>
2024-08-20 16:32:32 +0300

    Updated passwords on the dictionary

--- pw.py
+++ pw.py
@@ -2,4 +2,6 @@
 #Password Manager
 PASSWORDS = {'email': 'F7minlBDDuvMJuxESSKHFhTxFtjVB6',
              'blog': 'VmALvQyKAxiVH5G8v01if1MLZF3sdt',
-             'luggage': '12345'}
+             'luggage': '12345'
+             'bedroom': 'abracadabra'
+             'living room': 'wadnojani'}
